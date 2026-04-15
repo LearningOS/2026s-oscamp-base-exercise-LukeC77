06accdec1e34705c88a1d8224ee7f231b31a2cb4
mf090537@126.com <ycao12@Mecha-No1.localdomain>
2026-04-15 08:51:53 -0400

    complete channel

--- exercises/01_concurrency_sync/03_channel/src/lib.rs
+++ exercises/01_concurrency_sync/03_channel/src/lib.rs
@@ -18,7 +18,19 @@ pub fn simple_send_recv(items: Vec<String>) -> Vec<String> {
     // TODO: Spawn thread to send each element in items
     // TODO: In main thread, receive all messages and collect into Vec
     // Hint: When all Senders are dropped, recv() returns Err
-    todo!()
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        for item in items {
+            tx.send(item).unwrap();
+        }
+    });
+
+
+    let mut receiver = Vec::new();
+    for item in rx {
+        receiver.push(item);
+    }
+    receiver
 }
 
 /// Create `n_producers` producer threads, each sending a message in format `"msg from {id}"`.
@@ -30,7 +42,24 @@ pub fn multi_producer(n_producers: usize) -> Vec<String> {
     // TODO: Clone a sender for each producer
     // TODO: Remember to drop the original sender, otherwise receiver won't finish
     // TODO: Collect all messages and sort
-    todo!()
+    let (tx, rx) = mpsc::channel();
+    for i in 0..n_producers {
+        let tx_clone = tx.clone();
+        thread::spawn(move || {
+            let msg = format!("msg from {}", i);
+            tx_clone.send(msg).unwrap();
+        });
+    }
+    
+    drop(tx);
+
+    let mut messages = Vec::new();
+    for item in rx {
+        messages.push(item);
+    }
+
+    messages.sort();
+    messages
 }
 
 #[cfg(test)]
