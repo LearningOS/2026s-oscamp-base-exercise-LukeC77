c3a1b6dbe7dea0e19a54e6a2fe7cd82414d5ffe2
mf090537@126.com <ycao12@Mecha-No1.localdomain>
2026-04-19 13:25:27 -0400

    complete bump_allocator

--- exercises/02_no_std_dev/02_bump_allocator/src/lib.rs
+++ exercises/02_no_std_dev/02_bump_allocator/src/lib.rs
@@ -74,7 +74,24 @@ unsafe impl GlobalAlloc for BumpAllocator {
         // 5. Atomically update next to end using compare_exchange
         //    (if CAS fails, another thread raced — retry in a loop)
         // 6. Return the aligned address as a pointer
-        todo!()
+        loop {
+            let current_next = self.next.load(Ordering::SeqCst);
+            let align_next = (current_next + layout.align() - 1) 
+                & !(layout.align() - 1);
+            let alloc_end = align_next + layout.size();
+            if alloc_end > self.heap_end {
+                return null_mut();
+            }
+
+            if self.next.compare_exchange(
+                current_next,
+                alloc_end,
+                Ordering::SeqCst,
+                Ordering::SeqCst,
+            ).is_ok() {
+                return align_next as *mut u8;
+            }
+        }
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
